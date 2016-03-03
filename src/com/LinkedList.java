256461f451b48e54737879ff6843691c74a0896f
adarsh <adarshsa@thoughtworks.com>
2016-03-03 15:56:49 +0530

    refactored functions and made a new class named linked list and wrote some tests for it.

--- src/com/LinkedList.java
+++ src/com/LinkedList.java
@@ -8,19 +8,10 @@ public class LinkedList<K, V> {
     V value;
     LinkedList next = null;
 
-//    public LinkedList(K key,V value) {
-//        this.key = key;
-//        this.value = val
-//    }
-
-
-    public void setKey(K key){
+    public LinkedList(K key,V value) {
         this.key = key;
-    };
-
-    public void setValue(V value) {
         this.value = value;
-    };
+    }
 
     public void setNext(LinkedList next) {
         this.next = next;
